316882547664fcf4033186417ec15b6d974178a4
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2016-09-08 13:15:08 +0300

    pixified

--- bin/pixi-picture.js
+++ bin/pixi-picture.js
@@ -1,4 +1,11 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*!
+ * pixi-plugin-example - v1.0.1
+ * Compiled Thu Sep 08 2016 13:14:34 GMT+0300 (RTZ 2 (зима))
+ *
+ * pixi-plugin-example is licensed under the MIT License.
+ * http://www.opensource.org/licenses/mit-license
+ */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.pixiPicture = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var PictureShader = require('./PictureShader'),
     glCore = PIXI.glCore;
 
@@ -44,8 +51,8 @@ PictureRenderer.prototype.render = function(sprite) {
     if (!sprite.texture.valid) {
         return;
     }
-	//you can add different render modes here
-	//multiple shaders and stuff
+    //you can add different render modes here
+    //multiple shaders and stuff
     this._renderNormal(sprite, this.normalShader);
 };
 
@@ -56,14 +63,14 @@ PictureRenderer.prototype._renderNormal = function(sprite, shader) {
     var quad = this.quad;
     var uvs = sprite.texture._uvs;
 
-	//sprite already has calculated the vertices. lets transfer them to quad
+    //sprite already has calculated the vertices. lets transfer them to quad
     var vertices = quad.vertices;
     for (var i=0;i<8;i++) {
         quad.vertices[i] = sprite.vertexData[i];
     }
 
     //SpriteRenderer works differently, with uint32 UVS
-	//but for our demo float uvs are just fine
+    //but for our demo float uvs are just fine
     quad.uvs[0] = uvs.x0;
     quad.uvs[1] = uvs.y0;
     quad.uvs[2] = uvs.x1;
@@ -79,28 +86,28 @@ PictureRenderer.prototype._renderNormal = function(sprite, shader) {
     var frame = sprite.texture.frame;
     var base = sprite.texture.baseTexture;
     var clamp = this._tempClamp;
-	//clamping 0.5 pixel from each side to reduce border artifact
-	//this is our plugin main purpose
+    //clamping 0.5 pixel from each side to reduce border artifact
+    //this is our plugin main purpose
     clamp[0] = frame.x / base.width + 0.5 / base.realWidth;
     clamp[1] = frame.y / base.height + 0.5 / base.realWidth;
     clamp[2] = (frame.x + frame.width) / base.width - 0.5 / base.realWidth;
     clamp[3] = (frame.y + frame.height) / base.height - 0.5 / base.realWidth;
-	//take a notice that size in pixels is realWidth,realHeight
-	//width and height are divided by resolution
+    //take a notice that size in pixels is realWidth,realHeight
+    //width and height are divided by resolution
     shader.uniforms.uTextureClamp = clamp;
 
     var color = this._tempColor;
     PIXI.utils.hex2rgb(sprite.tint, color);
     var alpha = sprite.worldAlpha;
-	//premultiplied alpha tint
-	//of course we could do that in shader too
+    //premultiplied alpha tint
+    //of course we could do that in shader too
     color[0] *= alpha;
     color[1] *= alpha;
     color[2] *= alpha;
     color[3] = alpha;
     shader.uniforms.uColor = color;
 
-	//bind texture to unit 0, our default sampler unit
+    //bind texture to unit 0, our default sampler unit
     renderer.bindTexture(base, 0);
     quad.draw();
 };
@@ -122,15 +129,15 @@ function PictureShader(gl)
 {
     PIXI.Shader.call(this,
         gl,
-        "#define GLSLIFY 1\nattribute vec2 aVertexPosition;\n\nattribute vec2 aTextureCoord;\n\nattribute vec4 aColor;\n\nuniform mat3 projectionMatrix;\n\nvarying vec2 vTextureCoord;\n\nvoid main(void)\n\n{\n\n    gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);\n\n    vTextureCoord = aTextureCoord;\n\n}\n\n",
-        "#define GLSLIFY 1\nvarying vec2 vTextureCoord;\n\nvarying vec4 vColor;\n\nuniform sampler2D uSampler;\n\nuniform vec4 uTextureClamp;\n\nuniform vec4 uColor;\n\nvoid main(void)\n\n{\n\n    vec2 textureCoord = clamp(vTextureCoord, uTextureClamp.xy, uTextureClamp.zw);\n\n   \tvec4 sample = texture2D(uSampler, textureCoord);\n\n   \tgl_FragColor = sample * uColor;\n\n}\n\n"
+        "#define GLSLIFY 1\nattribute vec2 aVertexPosition;\nattribute vec2 aTextureCoord;\nattribute vec4 aColor;\n\nuniform mat3 projectionMatrix;\n\nvarying vec2 vTextureCoord;\n\nvoid main(void)\n{\n    gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);\n    vTextureCoord = aTextureCoord;\n}\n",
+        "#define GLSLIFY 1\nvarying vec2 vTextureCoord;\nvarying vec4 vColor;\n\nuniform sampler2D uSampler;\nuniform vec4 uTextureClamp;\nuniform vec4 uColor;\n\nvoid main(void)\n{\n    vec2 textureCoord = clamp(vTextureCoord, uTextureClamp.xy, uTextureClamp.zw);\n    vec4 sample = texture2D(uSampler, textureCoord);\n    gl_FragColor = sample * uColor;\n}\n"
     );
-	//do some stuff, like default values for shader
-	//dont forget to bind it if you really are changing the uniforms
-	this.bind();
-	//default tint
-	//Its an example, actually PictureRenderer takes care of this stuff
-	this.uniforms.uColor = new Float32Array(1,1,1,1);
+    //do some stuff, like default values for shader
+    //dont forget to bind it if you really are changing the uniforms
+    this.bind();
+    //default tint
+    //Its an example, actually PictureRenderer takes care of this stuff
+    this.uniforms.uColor = new Float32Array(1,1,1,1);
 }
 
 PictureShader.prototype = Object.create(PIXI.Shader.prototype);
@@ -163,15 +170,15 @@ module.exports = PictureSprite;
  */
 PictureSprite.prototype._renderWebGL = function (renderer)
 {
-	//copy of PIXI.Sprite v4 behaviour
+    //copy of PIXI.Sprite v4 behaviour
     if(this.transform.updated || this.textureDirty)
     {
         this.textureDirty = false;
         // set the vertex data
         this.calculateVertices();
     }
-	
-	//use different plugin for rendering
+
+    //use different plugin for rendering
     renderer.setObjectRenderer(renderer.plugins.picture);
     renderer.plugins.picture.render(this);
 };
@@ -188,7 +195,8 @@ Object.assign(PIXI.extras, myPlugin);
 
 module.exports = myPlugin;
 
-},{"./PictureRenderer":1,"./PictureSprite":3}]},{},[4])
+},{"./PictureRenderer":1,"./PictureSprite":3}]},{},[4])(4)
+});
 
 
 //# sourceMappingURL=pixi-picture.js.map
